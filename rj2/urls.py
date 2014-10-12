b4f9b7884a2c25ddc31ce66aa7e37b218743ffbb
Justin Perez <justin.f.perez@gmail.com>
2014-10-12 11:41:31 -0500

    Added all the urls, pointing them to the view stubs

--- rj2/urls.py
+++ rj2/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rj2 import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':
         'login.html'}),
+    url(r'^$', views.homepage),
+    url(r'^editaccount/$', views.editaccount),
+    url(r'^changepassword/$', views.changepassword),
 )
