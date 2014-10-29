469c2f6bd587033b03f873f57d83b6554664e823
Justin Perez <justin.f.perez@gmail.com>
2014-10-29 12:27:50 -0500

    fixed the check to see if a user is authenticated in the login template. also added an is_active flag to the course model.

--- rj2/models.py
+++ rj2/models.py
@@ -82,6 +82,7 @@ class Course(models.Model):
     description = models.TextField(blank=False, null=False)
     fee = models.DecimalField(decimal_places=2, max_digits=20)
     is_deprecated = models.BooleanField(blank=False, null=False, default=False)
+    is_active = models.BooleanField(blank=False, null=False, default=False)
 
 class Quiz(models.Model):
     course = models.ForeignKey(Course)
