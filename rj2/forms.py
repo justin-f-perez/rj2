f88d6cd55f820af117a89e2c884ea0f635ab2440
Justin Perez <justin.f.perez@gmail.com>
2014-10-31 14:54:39 -0500

    Initialize revision for course form.

---
+++ rj2/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from rj2.models import Course
+
+class CourseForm(ModelForm):
+    class Meta:
+        model = Course
+        fields = ['name', 'description', 'fee', 'instructors']
+
+
