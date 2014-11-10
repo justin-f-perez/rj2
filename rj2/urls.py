0cc5339bec7db4c2b60b2e62c002ffd1b4635b77
Justin Perez <justin.f.perez@gmail.com>
2014-11-10 14:38:46 -0600

    Added URLs and some views for add/edit quiz

--- rj2/urls.py
+++ rj2/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin, auth
+from django.views.generic.edit import CreateView
 from rj2 import views
+from rj2.models import Quiz
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -10,4 +12,8 @@ urlpatterns = patterns('',
     url(r'^manage_courses/(?P<pk>[0-9]+)/$', views.edit_course,
         name='edit_course'),
     url(r'^manage_courses/new/$', views.add_course, name='add_course'),
+    url(r'^manage_courses/(?P<pk>[0-9]+)/add_quiz/$',
+        views.add_quiz, name='add_quiz'),
+    url(r'^manage_courses/(?P<pk>[0-9]+)/add_quiz/(?P<quiz_id>[0-9]+)/$',
+        views.edit_quiz, name='edit_quiz'),
 )
--- rj2/views.py
+++ rj2/views.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from rj2.forms import CourseForm
-from rj2.models import Course
+from rj2.models import Course, Quiz
 
 
 @login_required
@@ -37,11 +37,29 @@ def add_course(request):
 
     if request.method == 'POST':
         form = form_class(request.POST)
+        form.instance.content_manager = request.user
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(template_name)
+            return HttpResponseRedirect('../')
         else:
             return render(request, template_name, {'form': form})
     else:
         form = form_class()
         return render(request, template_name, {'form': form})
+
+class QuizCreate(CreateView):
+    model = Quiz
+    fields = ['title',]
+
+    def dispatch(self, pk, *args, **kwargs):
+        self.course = get_object_or_404(Course, pk=pk)
+        return super().dispatch(*args, **kwargs)
+
+add_quiz = login_required(QuizCreate.as_view())
+
+class QuizUpdate(UpdateView):
+    model = Quiz
+    fields = ['title']
+    success_url = '/manage_courses'
+
+edit_quiz = login_required(QuizUpdate.as_view())
