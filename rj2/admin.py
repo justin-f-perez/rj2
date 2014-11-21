d1cbb034c8a3b18bb2d6df4b1d74dccf57e2410e
Justin Perez <justin.f.perez@gmail.com>
2014-11-21 14:48:16 -0600

    Added content manager to admin form for managing users.

--- rj2/admin.py
+++ rj2/admin.py
@@ -43,7 +43,8 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = MyUser
-        fields = ('email', 'password', 'is_active', 'is_admin')
+        fields = ('email', 'password', 'is_active', 'is_admin',
+        'is_content_manager')
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
@@ -60,11 +61,11 @@ class MyUserAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'is_admin')
-    list_filter = ('is_admin',)
+    list_display = ('email', 'is_admin', 'is_content_manager')
+    list_filter = ('is_admin', 'is_content_manager')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_content_manager')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
